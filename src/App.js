4527e52cd82de8c5f0caaddfd8245282325c1531
= <nastya.minty@gmail.com>
2022-10-29 15:32:22 +0300

    Front on React only

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleLogin } from '@react-oauth/google';
+// import { googleLogout } from '@react-oauth/google';
+// import { useGoogleOneTapLogin } from '@react-oauth/google';
+// import { useGoogleLogin } from '@react-oauth/google';
+import MyCustomButton from "./components/MyCustomButton";
+import { useGoogleLogin } from "@react-oauth/google";
+import Header from "./components/Header";
+import HeroCard from "./components/HeroCard";
+import { useRef } from "react";
+import AboutBlock from "./components/AboutBlock";
+import BigBlueButton from "./components/BigBlueButton";
+import LearningBlock from "./components/LearningBlock";
+import CodeWindow from "./components/CodeWindow";
+import BigCard from "./components/BigCard";
+import PartnersBlock from "./components/PartnersBlock";
+import Profile from "./profile";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useHistory,
+} from "react-router-dom";
+import { UserCircleIcon } from "@heroicons/react/solid";
 
 function App() {
+  // googleLogout();
+  const pageRefs = useRef({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId="1047839081489-v1v1dubc557qiqq36mhag4neskapnurk.apps.googleusercontent.com">
+      <div className="App">
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+
+        {/*<GoogleLogin*/}
+        {/*    onSuccess={credentialResponse => {*/}
+        {/*        console.log(credentialResponse);*/}
+        {/*    }}*/}
+        {/*    onError={() => {*/}
+        {/*        console.log('Login Failed');*/}
+        {/*    }}*/}
+        {/*/>*/}
+
+        {/* <MyCustomButton /> */}
+        <Header pageRefs={pageRefs} />
+        <main className="grid grid-cols-1 px-6 md:grid-cols-3">
+          <HeroCard img="solider-1.webp" title="Help Ukraine" />
+          <HeroCard
+            img="solider-1.webp"
+            title="Take Challenges"
+            //onClick={() => router.push("challenges")}
+          />
+          <HeroCard img="solider-1.webp" title="Get Job Offers" />
+        </main>
+        <AboutBlock pageRefs={pageRefs} />
+        <LearningBlock pageRefs={pageRefs} />
+        <PartnersBlock pageRefs={pageRefs} />
+        <img src="sponsors-1.png" />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
